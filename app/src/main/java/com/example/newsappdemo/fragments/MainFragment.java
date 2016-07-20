5a14ae89c8d04e36e592dbfd46edde6cc8370238
Rana Asif Bin Hamid <hemel018@gmail.com>
2016-07-20 12:30:50 +0600

    Service code committed.

--- app/src/main/java/com/example/newsappdemo/fragments/MainFragment.java
+++ app/src/main/java/com/example/newsappdemo/fragments/MainFragment.java
@@ -1,5 +1,9 @@
 package com.example.newsappdemo.fragments;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,15 +13,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-import android.content.SharedPreferences;
-import android.content.Context;
-
-import android.app.PendingIntent;
-import android.app.AlarmManager;
 
 import com.example.newsappdemo.R;
 import com.example.newsappdemo.adapters.NewsAdapter;
@@ -28,20 +28,21 @@ import com.example.newsappdemo.service.DownloadResultReceiver;
 import com.example.newsappdemo.service.NewsUpdateService;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 /**
  * Created by rana on 7/18/16.
  */
-public class MainFragment extends Fragment implements DownloadResultReceiver.Receiver  {
+public class MainFragment extends Fragment   {
 
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private DatabaseHandler mDatabaseHandler;
     private GregorianCalendar calendar;
     private PendingIntent pendingIntent;
+    private static ServiceReceiver serviceReceiver;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -85,72 +86,71 @@ public class MainFragment extends Fragment implements DownloadResultReceiver.Rec
 
     private void startServiceToUpdateNews() {
 
-        DownloadResultReceiver mReceiver = new DownloadResultReceiver(new Handler());
-        mReceiver.setReceiver(this);
-        Intent intent = new Intent(Intent.ACTION_SYNC, null, this.getActivity(), NewsUpdateService.class);
 
-        /* Send optional extras to Download IntentService */
-        String requestUrl = "http://197.157.246.110:3000/api/getupdate/";
-        intent.putExtra("url", requestUrl);
-        intent.putExtra("receiver", mReceiver);
-        intent.putExtra("requestId", 101);
+        serviceReceiver = new ServiceReceiver();
+        Intent alarmIntent = new Intent(this.getActivity(), AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this.getActivity(), 0, alarmIntent, 0);
 
-        AlarmManager alarmManager;
+
+
+
         calendar = (GregorianCalendar) Calendar.getInstance();
 
-        pendingIntent = PendingIntent.getService(this.getActivity(), 0,
-                intent, 0);
-
-        alarmManager = (AlarmManager) this.getActivity().getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis()+1000,
+        AlarmManager alarmManager = (AlarmManager) this.getActivity().getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis()+1000,
                 60*1000 , pendingIntent);
 
         //getActivity().startService(intent);
     }
 
-
-
-    @Override
-    public void onReceiveResult(int resultCode, Bundle resultData) {
-        switch (resultCode) {
-            case NewsUpdateService.STATUS_RUNNING:
-
-                getActivity().setProgressBarIndeterminateVisibility(true);
-                break;
-            case NewsUpdateService.STATUS_FINISHED:
-                /* Hide progress & extract result from bundle */
-                getActivity().setProgressBarIndeterminateVisibility(false);
-
-                NewsFeed[] results = (NewsFeed[])resultData.getParcelableArray("result");
-
-                /* Update ListView with result */
-
-                int count = 0;
-
-                for(NewsFeed news : results)
-                {
-
-                    mDatabaseHandler.addNewsFeed(news);
-                }
-
-                int index = 1;
-
-                for(Fragment fragment : ((ViewPagerAdapter)viewPager.getAdapter()).getmFragmentList())
-                {
-                    ((DataChangeListener)fragment).noitifyDataChangeListener(new NewsAdapter(mDatabaseHandler.getAllNewsByCategory(index)));
-                    index++;
-                }
-
-                //mRecyclerView.setAdapter(new NewsAdapter(mDatabaseHandler.getAllNewsByCategory(1)));
-
-                break;
-            case NewsUpdateService.STATUS_ERROR:
-                /* Handle the error */
-                String error = resultData.getString(Intent.EXTRA_TEXT);
-                Toast.makeText(this.getActivity(), error, Toast.LENGTH_LONG).show();
-                break;
-        }
+    public static ServiceReceiver getServiceReceiver()
+    {
+        return serviceReceiver;
     }
+
+
+
+//    @Override
+//    public void onReceiveResult(int resultCode, Bundle resultData) {
+//        switch (resultCode) {
+//            case NewsUpdateService.STATUS_RUNNING:
+//
+//                getActivity().setProgressBarIndeterminateVisibility(true);
+//                break;
+//            case NewsUpdateService.STATUS_FINISHED:
+//                /* Hide progress & extract result from bundle */
+//                getActivity().setProgressBarIndeterminateVisibility(false);
+//
+//                NewsFeed[] results = (NewsFeed[])resultData.getParcelableArray("result");
+//
+//                /* Update ListView with result */
+//
+//                int count = 0;
+//
+//                for(NewsFeed news : results)
+//                {
+//
+//                    mDatabaseHandler.addNewsFeed(news);
+//                }
+//
+//                int index = 1;
+//
+//                for(Fragment fragment : ((ViewPagerAdapter)viewPager.getAdapter()).getmFragmentList())
+//                {
+//                    ((DataChangeListener)fragment).noitifyDataChangeListener(new NewsAdapter(mDatabaseHandler.getAllNewsByCategory(index)));
+//                    index++;
+//                }
+//
+//                //mRecyclerView.setAdapter(new NewsAdapter(mDatabaseHandler.getAllNewsByCategory(1)));
+//
+//                break;
+//            case NewsUpdateService.STATUS_ERROR:
+//                /* Handle the error */
+//                String error = resultData.getString(Intent.EXTRA_TEXT);
+//                Toast.makeText(this.getActivity(), error, Toast.LENGTH_LONG).show();
+//                break;
+//        }
+//    }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
@@ -192,5 +192,76 @@ public class MainFragment extends Fragment implements DownloadResultReceiver.Rec
         ft.replace(id, fragment, fragment.toString());
         //ft.addToBackStack(null);
         ft.commit();
+    }
+
+    public static  class AlarmReceiver extends BroadcastReceiver {
+
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // For our recurring task, we'll just display a message
+
+            DownloadResultReceiver mReceiver = new DownloadResultReceiver(new Handler());
+            mReceiver.setReceiver(MainFragment.getServiceReceiver());
+            Intent serviceIntent = new Intent(Intent.ACTION_SYNC, null, context, NewsUpdateService.class);
+
+
+            String requestUrl = "http://197.157.246.110:3000/api/getupdate/";
+            serviceIntent.putExtra("url", requestUrl);
+            serviceIntent.putExtra("receiver", mReceiver);
+            serviceIntent.putExtra("requestId", 101);
+
+            context.startService(serviceIntent);
+
+            Log.d("test", "-------------------test");
+        }
+
+
+    }
+
+    class ServiceReceiver implements DownloadResultReceiver.Receiver
+    {
+
+        @Override
+        public void onReceiveResult(int resultCode, Bundle resultData) {
+            switch (resultCode) {
+                case NewsUpdateService.STATUS_RUNNING:
+
+                    getActivity().setProgressBarIndeterminateVisibility(true);
+                    break;
+                case NewsUpdateService.STATUS_FINISHED:
+                /* Hide progress & extract result from bundle */
+                    getActivity().setProgressBarIndeterminateVisibility(false);
+
+                    NewsFeed[] results = (NewsFeed[])resultData.getParcelableArray("result");
+
+                /* Update ListView with result */
+
+                    int count = 0;
+
+                    for(NewsFeed news : results)
+                    {
+
+                        mDatabaseHandler.addNewsFeed(news);
+                    }
+
+                    int index = 1;
+
+                    for(Fragment fragment : ((ViewPagerAdapter)viewPager.getAdapter()).getmFragmentList())
+                    {
+                        ((DataChangeListener)fragment).noitifyDataChangeListener(new NewsAdapter(mDatabaseHandler.getAllNewsByCategory(index)));
+                        index++;
+                    }
+
+                    //mRecyclerView.setAdapter(new NewsAdapter(mDatabaseHandler.getAllNewsByCategory(1)));
+
+                    break;
+                case NewsUpdateService.STATUS_ERROR:
+                /* Handle the error */
+                    String error = resultData.getString(Intent.EXTRA_TEXT);
+                    Toast.makeText(MainFragment.this.getActivity(), error, Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
     }
 }
